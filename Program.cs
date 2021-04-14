36521d85cd649a4d158e81a7984a3ceb0fe987c1
RUIZ SANDOVAL KEVIN YAEL <78570278+RSKY18141051@users.noreply.github.com>
2021-04-14 13:09:18 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lexico3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Lexico l = new Lexico())
+                {
+                    while (!l.FinDeArchivo())
+                    {
+                        l.NextToken();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Token.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexico3
+{
+    class Token
+    {
+        public enum clasificaciones
+        {
+            identificador, numero, asignacion, inicializacion,
+            finSentencia, operadorLogico, operadorRelacional,
+            operadorTermino, operadorFactor, incrementoTermino,
+            incrementoFactor, cadena, operadorTernario, caracter,
+            tipoDato, zona, condicion, ciclo, inicioBloque, finBloque
+        }
+        private string contenido;
+        private clasificaciones clasificacion;
+
+        public void setContenido(string contenido)
+        {
+            this.contenido = contenido;
+        }
+
+        public void setClasificacion(clasificaciones clasificacion)
+        {
+            this.clasificacion = clasificacion;
+        }
+
+        public string getContenido()
+        {
+            return contenido;
+        }
+
+        public clasificaciones getClasificacion()
+        {
+            return clasificacion;
+        }
+    }
+}
